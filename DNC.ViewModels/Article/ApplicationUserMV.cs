54a9615d1862abbb788e355c2289fcdd4bf9bf77
635096109 <635096109@qq.com>
2018-09-20 22:29:26 +0800

    用户权限管理

---
+++ DNC.ViewModels/Article/ApplicationUserMV.cs
@@ -0,0 +1,145 @@
+﻿using Dnc.Entities.Application;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DNC.ViewModels.Article
+{
+    public class ApplicationUserMV
+    {
+        public string ID { get; set; }
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        [StringLength(20)]
+        [Display(Name = "昵称")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [StringLength(20)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+        /// <summary>
+        /// 头像
+        /// </summary>
+        [Display(Name = "头像")]
+        public string Photo { get; set; }
+        /// <summary>
+        /// 手机号码
+        /// </summary>
+        [Display(Name = "手机号码")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 注册时间
+        /// </summary>
+        [Display(Name = "注册时间")]
+        public DateTime RegisterTime { get; set; }
+        /// <summary>
+        /// 最后访问时间
+        /// </summary>
+        [Display(Name = "最后访问时间")]
+        public DateTime LastVisitTime { get; set; }
+        /// <summary>
+        /// 安全问题
+        /// </summary>
+        [Display(Name = "安全问题")]
+        public string Safeques { get; set; }
+        /// <summary>
+        /// 安全问题答案
+        /// </summary>
+        [Display(Name = "安全问题答案")]
+        public string SafeAnswer { get; set; }
+        /// <summary>
+        /// 是否锁定
+        /// </summary>
+        [Display(Name = "是否被封号")]
+        public bool Locked { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [Display(Name = "邮箱")]
+        public string Email { get; set; }
+        /// <summary>
+        /// 用户积分
+        /// </summary>
+        [Display(Name = "用户积分")]
+        public int Integral { get; set; }
+        /// <summary>
+        /// 出生日期
+        /// </summary>
+        [Display(Name = "出生日期")]
+        public DateTime BirthDate { get; set; }
+        public string ApplicationGroupID { get; set; }
+        public string ApplicationGroupName { get; set; }
+        /// <summary>
+        /// 是否封号
+        /// </summary>
+        public string isName { get; set; }
+        /// <summary>
+        /// 用户权限
+        /// </summary>
+        public  ApplicationGroup ApplicationGroup { get; set; }
+        public ApplicationUserMV() { }
+        public ApplicationUserMV(ApplicationUser db)
+        {
+            this.ID = db.ID;
+            this.Name = db.Name;
+            this.UserName = db.UserName;
+            this.Password = db.Password;
+            this.Photo = db.Photo;
+            this.Phone = db.Phone;
+            this.RegisterTime = db.RegisterTime;
+            this.LastVisitTime = db.LastVisitTime;
+            this.Safeques = db.Safeques;
+            this.SafeAnswer = db.SafeAnswer;
+            this.Locked = db.Locked;
+            this.Email = db.Email;
+            this.Integral = db.Integral;
+            this.BirthDate = db.BirthDate;
+            if (db.Locked)
+            {
+                this.isName = "是";
+            } else
+            {
+                this.isName = "否";
+            }
+            if (db.ApplicationGroup != null)
+            {
+                this.ApplicationGroup = db.ApplicationGroup;
+                this.ApplicationGroupID = db.ApplicationGroup.ID;
+                this.ApplicationGroupName = db.ApplicationGroup.Name;
+            }
+        }
+        public void Mapo(ApplicationUser db)
+        {
+            db.ID = this.ID;
+            db.Name = this.Name;
+            db.UserName = this.UserName;
+            db.Password = this.Password;
+            db.Photo = this.Photo;
+            db.Phone = this.Phone;
+            db.RegisterTime = this.RegisterTime;
+            db.LastVisitTime = this.LastVisitTime;
+            db.Safeques = this.Safeques;
+            db.SafeAnswer = this.SafeAnswer;
+            db.Locked = this.Locked;
+            db.Email = this.Email;
+            db.Integral = this.Integral;
+            db.BirthDate = this.BirthDate;
+            if (this.ApplicationGroup != null)
+            {
+                db.ApplicationGroup = this.ApplicationGroup;
+                db.ApplicationGroup.ID = this.ApplicationGroupID;
+                db.ApplicationGroup.Name = this.ApplicationGroupName;
+            }
+        }
+    }
+}
---
+++ Dnc.MvcApp/Controllers/ApplicationUserController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dnc.DataAccessRepository.Repositories;
+using Dnc.Entities.Application;
+using Dnc.MvcApp.ViewInjections;
+using DNC.ViewModels.Article;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Dnc.MvcApp.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private readonly IEntityRepository _Service;
+
+        public ApplicationUserController(IEntityRepository service)
+        {
+            _Service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 获取用户全部信息
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> UserInfoAll(int page = 1, int limit = 10)
+        {
+            string strTrpe = Request.Query["key[name]"] + "";
+            LayerPage<ApplicationUserMV> lp = new LayerPage<ApplicationUserMV>();
+            var data = new List<ApplicationUser>();
+            await Task.Run(() =>
+            {
+                lp.code = 0;
+                lp.msg = "";
+                data = _Service.GetAll<ApplicationUser>(m => m.ApplicationGroup).Where(m => m.Name.Contains(strTrpe)).OrderByDescending(b => b.Name).Skip((page - 1) * limit).Take(limit).ToList();
+                lp.count = _Service.GetAll<ApplicationUser>().ToList().Count();
+            });
+            List<ApplicationUserMV> Info = new List<ApplicationUserMV>();
+            foreach (var item in data)
+            {
+                Info.Add(new ApplicationUserMV(item));
+            }
+            lp.data = Info;
+            return Json(lp);
+        }
+
+        /// <summary>
+        /// 商品管理-修改
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var data = _Service.GetSingle<ApplicationUser>(Guid.Parse(id),m=>m.ApplicationGroup);
+            var dataVM = new ApplicationUserMV(data);
+            ViewData["group"] = _Service.GetAll<ApplicationGroup>().ToList();
+            return View(dataVM);
+        }
+        [HttpPost]
+        public IActionResult Edit(ApplicationUserMV model)
+        {
+            if (ModelState.IsValid)
+            {
+                var and  = HttpContext.Request.Form["sex"];
+                var and1 = HttpContext.Request.Form["sex1"];
+                var mm = _Service.GetSingleBy<ApplicationUser>(m => m.ID == model.ID);
+                if (and1.ToString()=="0")
+                    mm.Locked = true;
+                else
+                    mm.Locked = false;
+                var apgruop =_Service.GetSingleBy<ApplicationGroup>(m => m.ID == and);
+                mm.ApplicationGroup = apgruop;
+                _Service.EditAndSave<ApplicationUser>(mm);
+                return Redirect("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+            #region 到要经过执行controller里方法后 显示出页面。
+            //return RedirectToAction("Index");//可跳出本controller
+            //return RedirectToRoute(new {controller="Home",action="Index"});//可跳出本controller
+           // return RedirectToRoute(new { controller = "ApplicationUser", action = "Index" });
+            
+            //Response.Redirect("Index");//只能使用本controller下的方法名称。返回值为void
+            //return Redirect("Index");//只能使用本controller下的方法名称。
+            //return Content("<script>alert('操作成功。';window.location.href='/home/index')");
+            #endregion
+        }
+        [HttpPost]
+        public IActionResult Del(String id, [FromForm] string batchid)
+        {
+            var logonStatus = new LogonUserStatus
+            {
+                IsLogon = false,
+                Message = "删除失败！"
+            };
+            try
+            {
+                if (string.IsNullOrEmpty(batchid) && id != null)
+                {
+                    var data = _Service.GetSingleBy<ApplicationUser>(m => m.ID == id);
+                    _Service.DeleteAndSave<ApplicationUser>(data);
+                }
+                else
+                {
+                    // 将遵循 json 规格定义的对象数据字符串转换为C#对象
+                    var labelInfo = JsonConvert.DeserializeObject<List<ApplicationUser>>(batchid);
+                    foreach (var item in labelInfo)
+                    {
+                        var data = _Service.GetSingleBy<ApplicationUser>(m => m.ID == item.ID);
+                        _Service.Delete(data);
+                    }
+                    _Service.Save();
+                }
+
+                logonStatus.IsLogon = true;
+                logonStatus.Message = "删除成功";
+            }
+            catch (Exception)
+            {
+                logonStatus.IsLogon = false;
+                logonStatus.Message = "删除失败";
+            }
+
+            return Json(logonStatus);
+        }
+    }
+}
