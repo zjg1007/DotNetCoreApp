751f1454607fb5732572ad587112481cdff080ef
635096109 <635096109@qq.com>
2019-02-02 13:39:33 +0800

    22

--- Dnc.Services/Controllers/testController.cs
+++ Dnc.Services/Controllers/testController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Dnc.Services.Controllers
 {
     /// <summary>
-    /// 测试接口
+    /// 测试
     /// </summary>
     [Produces("application/json")]
     [Route("api/[controller]")]
@@ -20,30 +20,29 @@ namespace Dnc.Services.Controllers
     {
         private readonly IEntityRepository _DbService;
         /// <summary>
-        /// 签到表
+        /// 购物车表
         /// </summary>
         /// <param name="service"></param>
         public TestController(IEntityRepository service)
         {
             this._DbService = service;
         }
-        // GET: api/<controller>
         /// <summary>
-        ///获取测试数据
+        /// 测试数据
         /// </summary>
         /// <returns></returns>
+        // GET: api/<controller>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/<controller>/5
         /// <summary>
         /// 
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
+        // GET api/<controller>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
@@ -58,11 +57,11 @@ namespace Dnc.Services.Controllers
         public void Post([FromBody]string value)
         {
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="value"></param>
+/// <summary>
+/// 
+/// </summary>
+/// <param name="id"></param>
+/// <param name="value"></param>
         // PUT api/<controller>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
